b9fc43a011207800df57e1d46b019e3b98cf3e71
Toni <loggerhard@yandex.ru>
2023-11-21 20:31:21 +0300

    5-6

---
+++ practic_6/4/WebApILinux/WebApILinux/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApILinux.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
